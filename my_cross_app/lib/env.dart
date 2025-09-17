cea6f1e0efeb6fc7a823efcbeaff3931505a8598
andimsewon <145431156+andimsewon@users.noreply.github.com>
2025-09-17 15:54:08 +0900

    위젯수정

--- my_cross_app/lib/env.dart
+++ my_cross_app/lib/env.dart
@@ -1,7 +1,11 @@
+import 'dart:io' show Platform;
+
 class Env {
-  // 웹/맥은 127.0.0.1, 안드로이드 에뮬은 10.0.2.2
-  static const proxyBase = String.fromEnvironment(
-    'PROXY_BASE',
-    defaultValue: 'http://127.0.0.1:8080',
-  );
+  static String get proxyBase {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080';
+    } else {
+      return 'http://127.0.0.1:8080';
+    }
+  }
 }
