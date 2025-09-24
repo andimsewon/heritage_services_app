89626584ae950c58c0481c6c1eed8835c39ad4b5
andimsewon <145431156+andimsewon@users.noreply.github.com>
2025-09-24 17:01:45 +0900

    firebase dong-gi-hwa

--- server/.venv/bin/activate.fish
+++ server/.venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kimsewon/AndroidStudioProjects/무제 폴더/server/.venv"
+set -gx VIRTUAL_ENV "/Users/kimsewon/AndroidStudioProjects/heritage_services_app/server/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
