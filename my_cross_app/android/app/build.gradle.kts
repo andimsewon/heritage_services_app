281c8a7b9030988b8ed57dac792dde0aa1f52655
andimsewon <145431156+andimsewon@users.noreply.github.com>
2025-09-25 13:28:04 +0900

    firebase dong-gi-hwa

--- my_cross_app/android/app/build.gradle.kts
+++ my_cross_app/android/app/build.gradle.kts
@@ -17,9 +17,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_11
     }
 
     defaultConfig {
